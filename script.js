fe02255e64e2bc4af8b9c98b53cb87b795150778
NATANAEL VEGA <75705151+Natanael-Vega@users.noreply.github.com>
2024-01-04 05:47:10 +0000

    Cambio en boton CV

--- script.js
+++ script.js
@@ -39,3 +39,17 @@ function efectoHabilidades(){
 window.onscroll = function(){
     efectoHabilidades();
 } 
+
+/*Botton*/
+const button = document.querySelector(".btn");
+const inner = document.querySelector(".inner");
+
+button.addEventListener("mousemove", (e) => {
+  const rect = button.getBoundingClientRect();
+
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  button.style.setProperty("--mouseX", `${x}px`);
+  button.style.setProperty("--mouseY", `${y}px`);
+});
